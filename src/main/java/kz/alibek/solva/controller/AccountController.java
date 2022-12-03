31ceafc7b750c14fa162a01c4b95089cb50113ff
Alibek <venommega20012@gmail.com>
2022-12-04 03:56:54 +0600

    Added controller for accounts

---
+++ src/main/java/kz/alibek/solva/controller/AccountController.java
@@ -0,0 +1,37 @@
+package kz.alibek.solva.controller;
+
+import kz.alibek.solva.model.entity.Account;
+import kz.alibek.solva.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/add")
+    @ResponseBody
+    public ResponseEntity createAccount(@RequestBody Account account){
+        try{
+            accountService.createAccount(account);
+            return ResponseEntity.ok("Account was created");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity getAll(){
+        try{
+            return ResponseEntity.ok(accountService.accountList());
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+}
