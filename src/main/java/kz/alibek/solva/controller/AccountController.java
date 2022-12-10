c28622cc11991eee762b200f1b1fea511f500e9f
Alibek <venommega20012@gmail.com>
2022-12-11 04:28:34 +0600

    Added exception handler class

--- src/main/java/kz/alibek/solva/controller/AccountController.java
+++ src/main/java/kz/alibek/solva/controller/AccountController.java
@@ -1,7 +1,6 @@
 package kz.alibek.solva.controller;
 
 import kz.alibek.solva.model.dto.AccountDto;
-import kz.alibek.solva.model.entity.Account;
 import kz.alibek.solva.service.AccountService;
 import kz.alibek.solva.service.AccountTransactionService;
 import lombok.RequiredArgsConstructor;
@@ -18,22 +17,14 @@ public class AccountController {
 
     @PostMapping("/add")
     @ResponseBody
-    public ResponseEntity createAccount(@RequestBody AccountDto account){
-        try{
-            accountService.createAccount(account);
-            return ResponseEntity.ok("Account was created");
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity createAccount(@RequestBody AccountDto account) {
+        accountService.createAccount(account);
+        return ResponseEntity.ok("Account was created");
     }
 
     @GetMapping("/")
-    public ResponseEntity getAll(){
-        try{
-            return ResponseEntity.ok(accountService.accountList());
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity getAll() {
+        return ResponseEntity.ok(accountService.accountList());
     }
 
     @PostMapping("/transfer")
@@ -41,32 +32,20 @@ public class AccountController {
     public ResponseEntity makeTransfer(@RequestParam(name = "fromAccount") String account1,
                                        @RequestParam(name = "toAccount") String account2,
                                        @RequestParam(name = "sum") float sum,
-                                       @RequestParam(name = "currency") String currency){
-        try{
-            accountTransactionService.transferMoney(account1, account2, sum, currency);
-            return ResponseEntity.ok("Transfer good");
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+                                       @RequestParam(name = "currency") String currency) {
+        accountTransactionService.transferMoney(account1, account2, sum, currency);
+        return ResponseEntity.ok("Transfer good");
     }
 
     @PostMapping("/limit")
     @ResponseBody
-    public ResponseEntity getAllLimitTransactions(@RequestParam(name = "accountNumber") String accountNumber){
-        try{
-            return ResponseEntity.ok(accountTransactionService.showAllTransactions(accountNumber));
-        } catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity getAllLimitTransactions(@RequestParam(name = "accountNumber") String accountNumber) {
+        return ResponseEntity.ok(accountTransactionService.showAllTransactions(accountNumber));
     }
 
     @PutMapping("/changeLimit/{accountId}")
     @ResponseBody
     public ResponseEntity addBook(@PathVariable(name = "accountId") int id, @RequestParam(name = "newLimit") int newLimit) {
-        try {
-            return ResponseEntity.ok(accountService.changeLimit(Long.valueOf(id), newLimit));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error");
-        }
+        return ResponseEntity.ok(accountService.changeLimit(Long.valueOf(id), newLimit));
     }
 }
